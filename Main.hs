ac184f272bf8321c52923b2d53e2666344357fc2
Michal Parusinski <mparusinski@gmail.com>
2011-12-12 21:44:02 +0000

    Changes

--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ Portability :  portable
 module Main where
 
 import Control.Monad
-import System.CPUTime
+import System.Time
 import System.Random
 
 import Factoring.Lenstra
@@ -22,29 +22,42 @@ import Primes.MillerRabin
 import Generator.RandomGenerator
 import Generator.Generator
 
+getTimeMS 
+    = do (TOD s p) <- getClockTime
+         return $ fromIntegral (s * 1000 + p `div` 10^6)
+
 performTrialFactoring num
-    = do before <- getCPUTime
+    = do before <- getTimeMS
+         factor <- return $! lenstraECMSmartBound num
+         after  <- getTimeMS
+         let diff = after - before
+         return (factor, diff)
+
+performTrialFactoringParallel num
+    = do before <- getTimeMS
          factor <- return $! lenstraECMParallelSmartBound num
-         after  <- getCPUTime
-         let resolution = fromIntegral cpuTimePrecision :: Double
-         diffTime <- return $! fromIntegral (after - before) / (100 * resolution)
-         return (factor, diffTime)
+         after  <- getTimeMS
+         let diff = after - before
+         return (factor, diff)
 
 generateProductTwoPrimes bitSize
-    = do -- updateIORandomGenerator
-         stdGen1 <- return $! mkStdGen 3 --getStdGen
+    = do updateIORandomGenerator
+         stdGen1 <- getStdGen
          let pg = primeGenerator stdGen1 bitSize
-         --updateIORandomGenerator
-         stdGen2 <- return $! mkStdGen 5 --getStdGen
+         updateIORandomGenerator
+         stdGen2 <- getStdGen
          let ([prime1, prime2], state) = runGeneratorNTimes 2 pg stdGen2
          return (prime1 * prime2, prime1, prime2)
 
-chosenBitSize = 60 -- bits
+chosenBitSize = 30 -- bits
 
 main = do (product, prime1, prime2) <- generateProductTwoPrimes chosenBitSize
-          putStrLn $ show product ++ " = " ++ show prime1 ++ " x " ++ show prime2
+          putStrLn $ show product++" = "++show prime1++" x "++show prime2
           (factor, diffTime) <- performTrialFactoring product
-          putStrLn $ show factor ++ " divides " ++ show product
-          putStrLn $ "Factor found in " ++ show diffTime ++ " sec"
+          putStrLn $ show factor++" divides "++show product
+          putStrLn $ "Factor found in "++show diffTime++" sec (not parallel)"
+          (factor2, diffTime2) <- performTrialFactoringParallel product
+          putStrLn $ show factor++" divides "++show product
+          putStrLn $ "Factor found in "++show diffTime++" sec (parallel)"
 
 
